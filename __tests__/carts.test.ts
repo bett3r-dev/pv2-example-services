35cb88c06e21a9745d5bc9816353deca0fa66ec9
Tomas Ruiz <tomasruizr@gmail.com>
2021-12-27 17:55:01 -0300

    [wip] integration tests

--- __tests__/carts.test.ts
+++ __tests__/carts.test.ts
@@ -1,3 +1,53 @@
-import {start} from '../../server';
+import { CartCommands } from '@bett3r-dev/pv2-example-domain';
+import * as u from 'src/app-utils';
+import { AppServerComponents } from 'src/types';
+import { start } from '../../server';
+import { CartsAggregate } from '../carts/carts.aggregate';
 
-start()
+const mockUpsert = jest.fn();
+const mockUpdate = jest.fn();
+const mockCreate = jest.fn();
+const mockDestroy = jest.fn();
+
+describe('description', () => {
+  let serverComponents: AppServerComponents;
+
+  beforeAll((done) => {
+    start({
+      providers:{
+        database:{
+          mongo:{
+            getCollection: () => ({
+              upsert: mockUpsert,
+              update: mockUpdate,
+              create: mockCreate,
+              destroy: mockDestroy
+            })
+          }
+        }
+      }
+    })
+      .map(({serverComponents: _serverCompoennts}) => {
+        serverComponents = _serverCompoennts;
+        _serverCompoennts.modulesLoadedStream.map(()=> done())
+      })
+      .fork(u.I, u.I)
+  })
+  it('description', () => {
+    // console.log(serverComponents.configuration.getConfig());
+    // console.log(serverComponents);
+    // expect(true).toBeTruthy();
+    const es = serverComponents.eventsourcing;
+    es.executeCommand(CartsAggregate({serverComponents, u}), CartCommands.CreateUserCart)({
+      params:{id:'438b476c-3a1e-45bc-bb43-c5c33798bd2c'},
+      body:{
+        "sku": "THE SKU TAL",
+        "name": "Some Product",
+        "price": 1234,
+        "quantity": 500
+      }
+    }).map(() => {
+      // serverComponents.eventstores['memory']._store; //?
+    })
+  });
+});
